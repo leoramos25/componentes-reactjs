33c9fcb2f2c6a4c618e4ad04bd52a7b76cbb71f8
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-12-14 12:55:59 -0300

    End challenge

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/header.scss"
+
+interface IHeaderprops {
+  selectedGenreTitle: string
+}
+
+export function Header({ selectedGenreTitle }: IHeaderprops) {
+  return (
+    <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+  )
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import "../styles/sidebar.scss";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ISideBarProps {
+  sideBarHandleClickButton(genreId: number): void;
+  sideBarSelectedGenreId: number;
+
+}
+
+export function SideBar({ sideBarHandleClickButton, sideBarSelectedGenreId }: ISideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => sideBarHandleClickButton(genre.id)}
+            selected={sideBarSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
